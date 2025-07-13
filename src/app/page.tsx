e82d35c60bfd37ed7dd17dc36f22560e453acad4
Wajeeh Haider <swajeeh03@gmail.com>
2025-07-14 04:43:40 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
+          className="dark:invert bg-secondary"
           src="/next.svg"
           alt="Next.js logo"
           width={180}
@@ -14,16 +14,21 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+            Get started by editing{' '}
+            <code className="bg-black/[.05] bg-primary px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               src/app/page.tsx
             </code>
             .
           </li>
-          <li className="tracking-[-.01em]">
+          <li className="tracking-[-.01em] bg-cream">
             Save and see your changes instantly.
           </li>
         </ol>
+
+        <h1 className="font-serif">This uses DM Serif Display</h1>
+        <p className="font-sans">This uses Poppins</p>
+        <p className="font-sans font-light">Poppins Light (300)</p>
+        <p className="font-sans font-semibold">Poppins Semibold (600)</p>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
