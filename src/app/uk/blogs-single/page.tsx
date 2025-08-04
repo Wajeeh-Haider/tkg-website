0c3e0f3fd0cf6bb96b56a19872a981cb70662615
SyedMuhammadRazaZaidi <109203958+SyedMuhammadRazaZaidi@users.noreply.github.com>
2025-08-04 16:38:19 +0500

    add blog single page UI desgin

---
+++ src/app/uk/blogs-single/page.tsx
@@ -0,0 +1,224 @@
+import Navbarhome from "@/components/uk/Homes/Navbarhome";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Footer from "@/components/Footer";
+const pages = () => {
+  return (
+    <div>
+      <Navbarhome country="uk" />
+      <div className="bg-[#E5F2F2]">
+        <div className="container mx-auto p-3 lg:p-0 lg:pt-67 pt-35 lg:pb-30 pb-10">
+          <div className="w-full lg:w-9/12">
+            <h1 className="text-[40px] lg:text-[72px] font-serif text-primary">
+              4 Essential Steps to Check If Your Pet Insurance Covers In-Home
+              Euthanasia
+            </h1>
+            <p className="text-[14px] lg:text-[17px] font-sans text-teal-800 pb-5 pt-3">
+              July 17, 2025
+            </p>
+            <a
+              href="#"
+              className="mb-5 text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+            >
+              Tips
+            </a>
+
+            {/* Tags section */}
+            <div className="flex flex-wrap gap-2 mt-4">
+              <a
+                href="#"
+                className="text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+              >
+                cat euthanasia at home
+              </a>
+              <a
+                href="#"
+                className="text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+              >
+                cat insurance claims
+              </a>
+              <a
+                href="#"
+                className="text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+              >
+                compassionate pet care
+              </a>
+              <a
+                href="#"
+                className="text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+              >
+                dog euthanasia at home
+              </a>
+              <a
+                href="#"
+                className="text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+              >
+                dog insurance coverage
+              </a>
+              <a
+                href="#"
+                className="text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+              >
+                euthanasia costs UK
+              </a>
+              <a
+                href="#"
+                className="text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+              >
+                euthanasia for pets UK
+              </a>
+              <a
+                href="#"
+                className="text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+              >
+                home vet services
+              </a>
+              <a
+                href="#"
+                className="text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+              >
+                in-home euthanasia
+              </a>
+              <a
+                href="#"
+                className="text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+              >
+                pet aftercare UK
+              </a>
+              <a
+                href="#"
+                className="text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+              >
+                pet euthanasia checklist
+              </a>
+              <a
+                href="#"
+                className="text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+              >
+                pet insurance UK
+              </a>
+              <a
+                href="#"
+                className="text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+              >
+                pet loss support
+              </a>
+              <a
+                href="#"
+                className="text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+              >
+                vet at home UK
+              </a>
+              <a
+                href="#"
+                className="text-sm border border-teal-600 text-teal-700 px-3 py-1 rounded-md hover:bg-primary hover:text-white transition"
+              >
+                pet end-of-life care
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* body */}
+        <div className="bg-[#FEFBF8] ">
+          <div className="container mx-auto p-3 lg:p-0">
+            <div className="flex flex-col lg:flex-row md:flex-row justify-between">
+              {/* Image and text */}
+              <div className="w-full md:w-1/2 ">
+                <Image
+                  src="/images/howitwork1.jpg"
+                  alt="How it works"
+                  width={700}
+                  height={700}
+                  className="relative lg:w-full w-[500px] lg:h-[800px] h-[300px] object-cover top-0 md:-top-7 lg:-top-15"
+                />
+                <h1 className="lg:text-[57.67px] text-[28px] md:text-[38.72px] text-[#404040] font-serif mb-5">
+                  What Is In-Home Euthanasia?
+                </h1>
+                <p className="text-[14px] lg:text-[19px] font-sans text-gray-500 leading-relaxed">
+                  In-home euthanasia is a service provided by licensed
+                  veterinarians who visit your home to perform the euthanasia
+                  procedure. Your pet remains in their safe, comfortable
+                  environment surrounded by familiar sights, sounds, and smells.
+                  This setting helps reduce anxiety and stress, allowing for a
+                  more peaceful goodbye.
+                </p>
+              </div>
+              {/* Recent Posts */}
+              <div>
+                <div className="bg-[#E5F2F2] p-6 mt-30 w-full max-w-md">
+                  <h2 className="lg:text-[32.16px] md:text-[26.88px] text-[22.88px] font-serif text-primary mb-4">
+                    Recent Posts
+                  </h2>
+
+                  <div className="space-y-6">
+                    <div>
+                      <Link href="#">
+                        <p className="font-sans font-medium text-gray-800 text-[15.72px] lg:text-[19.125px] hover:underline decoration-[1px] underline-offset-8 cursor-pointer">
+                          10 Powerful Signs Your Dog Is Suffering This Summer:
+                          How to Understand When End of Life Is Near
+                        </p>
+                      </Link>
+                      <p className="text-[14px] lg:text-[17px] font-sans text-teal-800 pb-3 pt-2">
+                        July 17, 2025
+                      </p>
+                    </div>
+
+                    <div>
+                      <Link href="#">
+                        <p className="font-sans font-medium text-gray-800 text-[15.72px] lg:text-[19.125px] hover:underline decoration-[1px] underline-offset-8 cursor-pointer">
+                          4 Essential Steps to Check If Your Pet Insurance
+                          Covers In-Home Euthanasia
+                        </p>
+                      </Link>
+                      <p className="text-[14px] lg:text-[17px] font-sans text-teal-800 pb-3 pt-2">
+                        March 27, 2025
+                      </p>
+                    </div>
+
+                    <div>
+                      <Link href="#">
+                        <p className="font-sans font-medium text-gray-800 text-[15.72px] lg:text-[19.125px] hover:underline decoration-[1px] underline-offset-8 cursor-pointer">
+                          How to Arrange Home Euthanasia for Your Dog?
+                        </p>
+                      </Link>
+                      <p className="text-[14px] lg:text-[17px] font-sans text-teal-800 pb-3 pt-2">
+                        June 3, 2024
+                      </p>
+                    </div>
+
+                    <div>
+                      <Link href="#">
+                        <p className="font-sans font-medium text-gray-800 text-[15.72px] lg:text-[19.125px] hover:underline decoration-[1px] underline-offset-8 cursor-pointer">
+                          Why In-Home Pet Euthanasia Can Be the Kindest Choice
+                          for Your Pet
+                        </p>
+                      </Link>
+                      <p className="text-[14px] lg:text-[17px] font-sans text-teal-800 pb-3 pt-2">
+                        April 8, 2024
+                      </p>
+                    </div>
+
+                    <div>
+                      <Link href="#">
+                        <p className="font-sans font-medium text-gray-800 text-[15.72px] lg:text-[19.125px] hover:underline decoration-[1px] underline-offset-8 cursor-pointer">
+                          Should You Stay With Your Pet During Euthanasia?
+                        </p>
+                      </Link>
+                      <p className="text-[14px] lg:text-[17px] font-sans text-teal-800 pb-3 pt-2">
+                        October 3, 2023
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default pages;
